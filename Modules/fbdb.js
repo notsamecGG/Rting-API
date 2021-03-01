6acf1f9db8ef58b959c79b336973d12b3f180a36
Sam <samuel.grulich@gmail.com>
2021-03-01 22:30:55 +0100

    FBDB bugfix 🐞🔥
    Still don't know what was wrong, but it works!

--- Modules/fbdb.js
+++ Modules/fbdb.js
@@ -1,22 +1,29 @@
-const firebase = require('firebase');
+const firebase = require('firebase/app');
+require('firebase/database');
 require('dotenv').config();
 
 module.exports = { InitDB, Init };
 
-async function InitDB() {
-    await firebase.initializeApp({ 
+function _defaultInit() {
+    const config = { 
         apiKey: process.env.API_KEY,
         authDomain: process.env.AUTH_DOMAIN,
         projectId: process.env.PROJECT_ID,
         storageBucket: process.env.STORAGE_BUCKET,
         messagingSenderId: process.env.MESSAGING_SENDER_ID,
         appId: process.env.APP_ID,
-        measurementId: process.env.MEASUREMENT_ID
-    });
+        // measurementId: process.env.MEASUREMENT_ID //optional
+    };
 
+    firebase.initializeApp(config);
+}
+
+async function InitDB() {
+    await _defaultInit();
     return await firebase.database();
 }
 
-async function Init(path) {
-    return InitDB().ref(path);
+async function Init(path){
+    await _defaultInit();
+    return await firebase.database().ref();
 }
